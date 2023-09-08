10bc4254bf2935580f900dcad83501dfcaa6704e
Fenil <107786545+fenilsonani@users.noreply.github.com>
2023-09-08 18:42:39 +0530

    update buid file

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
  echo "BUILD START"
- mkdir dist
- echo "DIST CREATED"
  echo "INSTALLING REQUIREMENTS"
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
