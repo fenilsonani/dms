9af89cd3c66d8f5d8c5fc9966abed4350cc1ddbe
Fenil <107786545+fenilsonani@users.noreply.github.com>
2023-09-20 16:29:21 +0530

    updated the rowater

--- rowater/views.py
+++ rowater/views.py
@@ -2,6 +2,8 @@ import csv
 from django.http import HttpResponse
 from django.shortcuts import render
 from reportlab.pdfgen import canvas
+from .filters import CustomerFilter, DeliveryFilter
+from .resources import CustomerResource, DeliveryResource
 from .forms import CustomerForm, DeliveryForm, Customer, Delivery
 
 
@@ -103,115 +105,29 @@ def delete_delivery(request, id):
         return render(request, 'users/not_loggedin.html', {'message': 'You need to login first.'})
 
 
-def display_delivery(request):
-    if request.user.is_authenticated:
-        deliveries = Delivery.objects.all()
-
-        if request.method == 'POST':
-            print(request.POST)
-            export_format = request.POST.get('export_format')
-            if export_format == 'pdf':
-                response = generate_pdf(deliveries)
-            elif export_format == 'csv':
-                response = generate_csv(deliveries)
-            else:
-                response = HttpResponse("Unsupported format")
-
-            return response
-
-        return render(request, 'icechip/display/delivery.html', {'deliveries': deliveries})
-    else:
-        return render(request, 'users/not_loggedin.html', {'message': 'You need to login first.'})
-
-
-def generate_pdf(data):
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="deliveries.pdf"'
-
-    pdf = canvas.Canvas(response)
-
-    # Define your PDF layout and content here
-    for delivery in data:
-        pdf.drawString(100, 700, f'Delivery ID: {delivery.id}')
-        pdf.drawString(100, 680, f'Delivery Date: {delivery.customer}')
-        pdf.drawString(100, 660, f'Delivery Date: {delivery.date}')
-        pdf.drawString(100, 640, f'Delivery Date: {delivery.daily_ice_block_given}')
-        pdf.showPage()
-
-    pdf.save()
-    return response
-
-
-def generate_csv(data):
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="deliveries.csv"'
-
-    writer = csv.writer(response)
-    writer.writerow(['Delivery ID', 'Delivery Customer', 'Delivery Date', 'Delivery Ice Block Delivered',
-                     'Delivery One Ice Chip Price', 'Delivery Total Ice Chip Price',
-                     'Delivery Transportation Charge', 'Delivery Total Charge'])
-
-    # Write your CSV content here
-    for delivery in data:
-        writer.writerow([delivery.id, delivery.customer, delivery.date, delivery.daily_ice_block_given,
-                         delivery.one_ice_chip_price, delivery.total_ice_chip_price,
-                         delivery.additional_transportation_charge, delivery.total_charge])
-
-    return response
-
-
 def display_customer(request):
     if request.user.is_authenticated:
-        customers = Customer.objects.all()
+        customer_filter = CustomerFilter(request.GET, queryset=Customer.objects.all())
 
-        if request.method == 'POST':
-            print(request.POST)
-            export_format = request.POST.get('export_format')
-            if export_format == 'pdf':
-                response = generate_pdf1(customers)
-            elif export_format == 'csv':
-                response = generate_csv1(customers)
-            else:
-                response = HttpResponse("Unsupported format")
-
+        if 'export' in request.GET:
+            dataset = CustomerResource().export(queryset=customer_filter.qs)
+            response = HttpResponse(dataset.csv, content_type='text/csv')
             return response
 
-        return render(request, 'icechip/display/customer.html', {'customers': customers})
-    else:
-        return render(request, 'users/not_loggedin.html', {'message': 'You need to login first.'})
+        return render(request, 'rowater/display/customer.html', {'filter': customer_filter})
+
+    return render(request, 'users/not_loggedin.html', {'message': 'You need to login first.'})
 
 
-def generate_pdf1(data):
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="customers.pdf"'
+def display_delivery(request):
+    if request.user.is_authenticated:
+        delivery_filter = DeliveryFilter(request.GET, queryset=Delivery.objects.all())
 
-    pdf = canvas.Canvas(response)
+        if 'export' in request.GET:
+            dataset = DeliveryResource().export(queryset=delivery_filter.qs)
+            response = HttpResponse(dataset.csv, content_type='text/csv')
+            return response
 
-    # Define your PDF layout and content here
-    for customer in data:
-        pdf.drawString(100, 700, f'Customer ID: {customer.id}')
-        pdf.drawString(100, 680, f'Customer Name: {customer.name}')
-        pdf.drawString(100, 660, f'Customer Address: {customer.address}')
-        pdf.drawString(100, 640, f'Customer Phone Number: {customer.phone_number}')
-        pdf.drawString(100, 620, f'Customer Paid Amount: {customer.paid_amount}')
-        pdf.drawString(100, 600, f'Customer Pending Amount: {customer.pending_amount}')
-        pdf.showPage()
+        return render(request, 'rowater/display/delivery.html', {'filter': delivery_filter})
 
-    pdf.save()
-    return response
-
-
-def generate_csv1(data):
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="customers.csv"'
-
-    writer = csv.writer(response)
-    writer.writerow(['Customer ID', 'Customer Name', 'Customer Address', 'Customer Phone Number',
-                     'Customer Paid Amount', 'Customer Pending Amount'])
-
-    # Write your CSV content here
-    for customer in data:
-        writer.writerow([customer.id, customer.name, customer.address, customer.phone_number,
-                         customer.paid_amount, customer.pending_amount])
-
-    return response
+    return render(request, 'users/not_loggedin.html', {'message': 'You need to login first.'})
